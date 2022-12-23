4e0ac0fcfd97399c628bcfff2f65dfb5d9177bc4
thomas1117 <thomasshannon117@yahoo.com>
2022-12-22 22:24:03 -0800

    add tailwind config start

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+//tailwindcss.com/docs/installation
+https: module.exports = {
+  content: ['./main.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
